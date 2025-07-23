4b5c000b857f763b905d833aa777ff5af341fadd
Miles Smith <miles-smith@omrf.org>
2025-07-23 13:58:28 -0500

    update packaging

--- src/pyUCell/__init__.py
+++ src/pyUCell/__init__.py
@@ -1,1 +1,3 @@
 from .ucell import score_genes_ucell
+
+__all__ = ["score_genes_ucell"]
--- src/pyUCell/ucell.py
+++ src/pyUCell/ucell.py
@@ -5,7 +5,7 @@ from numba import njit
 @njit
 def minimal_ranker(arr, ascending=False):
     arr = np.ravel(arr)
-    sorter = np.argsort(arr, kind='quicksort')
+    sorter = np.argsort(arr, kind="quicksort")
 
     inv = np.empty(sorter.size, dtype=np.intp)
     inv[sorter] = np.arange(sorter.size, dtype=np.intp)
@@ -15,7 +15,7 @@ def minimal_ranker(arr, ascending=False):
 
     count = np.hstack((np.nonzero(obs)[0], np.array([len(obs)])))
     dense = obs.cumsum()[inv]
-    result = .5 * (count[dense] + count[dense - 1] + 1)
+    result = 0.5 * (count[dense] + count[dense - 1] + 1)
 
     if ascending:
         return result
@@ -39,7 +39,9 @@ def insert_ranks_of_nonzero(base_rank_arr, nonzero_arr, csr_indices):
 
 def rank_sparse(sp_arr):
     new_arr = get_ranks_of_zeros(sp_arr.shape[1], sp_arr.nnz)
-    insert_ranks_of_nonzero(new_arr, sp_arr[:, sp_arr.indices].toarray(), sp_arr.indices)
+    insert_ranks_of_nonzero(
+        new_arr, sp_arr[:, sp_arr.indices].toarray(), sp_arr.indices
+    )
     return new_arr
 
 
@@ -52,11 +54,18 @@ def _calculate_u_score(vec, max_rank, n_signature, idx):
     return auc
 
 
-def score_genes_ucell(adata, signature, max_rank=1500, score_name='ucell_score',  copy=False):
+def score_genes_ucell(
+    adata, signature, max_rank=1500, score_name="ucell_score", copy=False
+):
     adata = adata.copy() if copy else adata
     n_signature = len(signature)
     idx = [adata.var.index.to_list().index(s) for s in signature]
 
-    res = map(lambda vec: _calculate_u_score(vec, max_rank=max_rank, n_signature=n_signature, idx=idx), [vec for vec in adata.X])
+    res = map(
+        lambda vec: _calculate_u_score(
+            vec, max_rank=max_rank, n_signature=n_signature, idx=idx
+        ),
+        [vec for vec in adata.X],
+    )
     adata.obs[score_name] = list(res)
     return adata if copy else None
